0640131f10038ef05b61e07e8d43a329494633c0
Jagadeesh <74336885+0-jagadeesh-0@users.noreply.github.com>
2021-11-13 19:45:57 +0530

    Adding Landing Page

--- src/components/pages/Welcome/Welcome.jsx
+++ src/components/pages/Welcome/Welcome.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from 'react';
+import Box from '@mui/material/Box';
+// import background from '../../../images/welcome-background.png';
+import './welcome.scss';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { useHistory } from 'react-router';
+
 
 const Welcome = () => {
+
+  const history = useHistory();
+
+
   return (
-    <div>
-      welcome
-    </div>
+    <Box className="home">
+
+      <Box className="company">
+        <Typography className="company-name" variant="h1" component="h2" >
+          <i>INSTA<span>FOOD</span></i>
+        </Typography>
+        <Typography className="tag">
+          Order Here & Get the food instantly.
+        </Typography>
+
+        <Box className="users">
+          <Button
+            variant="contained"
+            className="user-btn"
+            onClick={() => { history.push("/signup/restaurant") }}
+          >
+            Add Restaurant
+          </Button>
+          <Button
+            variant="contained"
+            className="user-btn"
+            onClick={() => { history.push("/signup/user") }}
+          >
+            Make Order
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
-export default Welcome
+export default Welcome;
